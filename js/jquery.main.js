2db80262c259a5b7bae8038d4cc987f3a3b369b3
Rushma Maharjan <Rushma.Maharjan@omnicommediagroup.com>
2021-03-27 18:09:46 +0545

    cart page

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -3,7 +3,28 @@ jQuery(function() {
 	initCustomForms();
 	initBannerCarousel();
 	initStickyScrollBlock();
+	initFormValidation();
 });
+
+function initFormValidation() {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
 
 function initHideloginSignup() {
 	var options= {
